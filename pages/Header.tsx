daa6f1fb2784d2560d72c8ccb6bfd9da2b716b3a
Ans Christy <jovanacarmel@gmail.com>
2025-10-01 14:49:49 +0530

    feat: made navbar more interactive

--- pages/Header.tsx
+++ pages/Header.tsx
@@ -12,9 +12,15 @@ export default function Header() {
       <div
         className={`absolute top-0 right-0 ${spe.className} left-0 gap-6 sm:gap-8 md:gap-10 lg:gap-22 shadow-[0px_5px_8px_-5px_rgba(0,0,0,1)] text-2xl sm:text-3xl md:text-3xl lg:text-4xl bg-[rgb(255,255,157)] flex justify-center items-center shadow-amber-200 rounded-b-3xl`}
       >
-        <button className="text-black">Home</button>
-        <button className="text-black">Pics</button>
-        <button className="text-black">About</button>
+        <button className="text-black cursor-pointer p-4 hover:text-[rgb(255,255,157)] hover:bg-black">
+          Home
+        </button>
+        <button className="text-black cursor-pointer p-4 hover:text-[rgb(255,255,157)] hover:bg-black">
+          Pics
+        </button>
+        <button className="text-black cursor-pointer p-4 hover:text-[rgb(255,255,157)] hover:bg-black">
+          About
+        </button>
       </div>
     </>
   );
